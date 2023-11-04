369fccc2503b29222ee1923826f6258cb5df268b
Tevin Glover <tevwork.tg@gmail.com>
2023-11-04 18:04:40 -0400

    study time

--- src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
+++ src/main/java/com/zipcodewilmington/danny_do_better_exercises/StringUtilities.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.danny_do_better_exercises;
 
+import java.util.Arrays;
+
 /**
  * Created by dan on 6/14/17.
  */
@@ -10,70 +12,103 @@ public class StringUtilities {
     public static String getHelloWorld() {
 
 
-
-        return "hello World";
+        return "Hello World";
     }
 
     /**
-     * @param firstSegment a string to be added to
+     * @param firstSegment  a string to be added to
      * @param secondSegment a string to add
      * @return the concatenation of two strings, `firstSegment`, and `secondSegment`
      */
-    public static String concatenation(String firstSegment, String secondSegment){
+    public static String concatenation(String firstSegment, String secondSegment) {
 
 
-
+        return firstSegment.concat(secondSegment);
 
         //return null;
     }
 
     /**
-     * @param firstSegment a string to be added to
+     * @param firstSegment  a string to be added to
      * @param secondSegment a string to add
      * @return the concatenation of an integer, `firstSegment`, and a String, `secondSegment`
      */
-    public static String concatenation(int firstSegment, String secondSegment){
-        return null;
+    public static String concatenation(int firstSegment, String secondSegment) {
+
+
+        return firstSegment + secondSegment;
+        //return null;
     }
+
 
     /**
      * @param input a string to be manipulated
      * @return the first 3 characters of `input`
      */
-    public static String getPrefix(String input){
-        return null;
+    public static String getPrefix(String input) {
+
+        return input.substring(0, 3);
+
+        // return null;
     }
 
     /**
      * @param input a string to be manipulated
      * @return the last 3 characters of `input`
      */
-    public static String getSuffix(String input){
-        return null;
+    public static String getSuffix(String input) {
+        // i want to get the length of string.
+        int pre = input.length() - 3;
+        int fix = input.length();
+        // subtract the length by 3.
+        // create two int  pre,fix.
+        // and enter them into sub string.
+
+
+        return input.substring(pre, fix);
+        //return null;
     }
 
     /**
-     * @param inputValue the value to be compared
+     * @param inputValue      the value to be compared
      * @param comparableValue the value to be compared against
      * @return the equivalence of two strings, `inputValue` and `comparableValue`
      */
-    public static Boolean compareTwoStrings(String inputValue, String comparableValue){
-        return null;
+    public static Boolean compareTwoStrings(String inputValue, String comparableValue) {
+        if (inputValue == comparableValue) {
+            return true;
+        } else
+            return false;
     }
 
     /**
      * @param inputValue the value input from user
      * @return the middle character of `inputValue`
      */
-    public static Character getMiddleCharacter(String inputValue){
-        return null;
+    public static Character getMiddleCharacter(String inputValue) {
+        int middle = inputValue.length() / 2;
+        // take the number and in the string.
+        return inputValue.charAt(middle-1);
+        //return;
+        //return null;
     }
+
+
 
     /**
      * @param spaceDelimitedString a string, representative of a sentence, containing spaces
      * @return the first sequence of characters
      */
     public static String getFirstWord(String spaceDelimitedString){
+        // spilting a string but it space and returning the first sub string
+        String[] spaceSpliter = spaceDelimitedString.split("");
+
+        for(int i = 0; i<= spaceSpliter.length; i++){
+            System.out.println( spaceSpliter[i]);
+
+        }
+
+
         return null;
     }
 
